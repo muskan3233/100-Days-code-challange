d034e5c6e90053b7fd2ba2493d76cc8cc76d9be9
Rehmat <95978634+muskan3233@users.noreply.github.com>
2023-07-18 12:32:23 +0530

    Create G4G Potd.java
    Day2-8th july 2023

---
+++ Day-2/G4G Potd.java
@@ -0,0 +1,13 @@
+// Stock buy and sell II
+class Solution {
+   public static int stockBuyAndSell(int n, int[] prices) {
+        // code here
+        int res=0;
+        for(int i=1;i<n;i++){
+            if(prices[i]>prices[i-1])
+            res+=prices[i]-prices[i-1];
+        }
+        return res;
+    }
+}
+// https://practice.geeksforgeeks.org/problems/stock-buy-and-sell2615/1
