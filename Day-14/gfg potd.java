36a7f553182096625286d2c7c96cdcf761486eb1
Rehmat <95978634+muskan3233@users.noreply.github.com>
2023-07-19 19:15:38 +0530

    Create gfg potd.java

---
+++ Day-14/gfg potd.java
@@ -0,0 +1,53 @@
+//First non-repeating character in a stream
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String A = br.readLine().trim();
+            Solution ob = new Solution();
+            String ans = ob.FirstNonRepeating(A);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public String FirstNonRepeating(String A) {
+        int[] charCount = new int[26];
+        Queue<Character> order = new LinkedList<>();
+        StringBuilder result = new StringBuilder();
+
+        for (char c : A.toCharArray()) {
+            int index = c - 'a';
+            charCount[index]++;
+            order.add(c);
+
+            while (!order.isEmpty() && charCount[order.peek() - 'a'] > 1) {
+                order.poll();
+            }
+
+            if (order.isEmpty()) {
+                result.append('#');
+            } else {
+                result.append(order.peek());
+            }
+        }
+
+        return result.toString();
+    }
+}
+
+//https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
